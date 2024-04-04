61d094c074196fa2fe2439a8880a8c46d8057080
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-04 16:44:04 +0200

    Add avatar do custom user Changes to be committed: 	modified:   src/components/layout/current-user.tsx

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,6 +1,12 @@
 import { Popover } from 'antd'
+import CustomAvatar from '../custom-avatar'
+import { useGetIdentity } from '@refinedev/core'
+
+import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>()
+
   return (
     <>
       <Popover
@@ -8,7 +14,14 @@ const CurrentUser = () => {
         trigger='click'
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
-      ></Popover>
+      >
+        <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size='default'
+          style={{ cursor: 'pointer' }}
+        />
+      </Popover>
     </>
   )
 }
