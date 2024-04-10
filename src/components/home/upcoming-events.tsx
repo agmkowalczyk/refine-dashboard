0dd30518ac5d65831e50c0595863c00113ee6ee9
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-10 20:50:27 +0200

    Card in Upcoming Events Changes to be committed: 	modified:   src/components/home/upcoming-events.tsx

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,6 +1,24 @@
+import { CalendarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
+import { Text } from '../text'
+
 const UpcomingEvents = () => {
   return (
-    <div>UpcomingEvents</div>
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '0 1rem' }}
+      title={
+        <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <CalendarOutlined />
+          <Text size='sm' style={{ marginLeft: '0.7rem' }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      Upcoming Events
+    </Card>
   )
 }
 
