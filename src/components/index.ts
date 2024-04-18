589de85e28f695fdbeb89184eccda3ef9f9f295c
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-19 00:46:21 +0200

    DashboardTotalCountCard to index Changes to be committed: 	modified:   src/components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import DealsChart from './home/deals-chart'
 import UpcomingEvents from './home/upcoming-events'
 import UpcomingEventsSkeleton from './skeleton/upcoming-events'
+import DashboardTotalCountCard from './home/total-count-card'
 
-export { DealsChart, UpcomingEvents, UpcomingEventsSkeleton }
+export { DealsChart, UpcomingEvents, UpcomingEventsSkeleton, DashboardTotalCountCard }
