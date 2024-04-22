44fd9d56fa3bfa42a6a5fbc9f92de57a02557302
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-22 16:22:32 +0200

    Edit DashboardTotalCountCard Changes to be committed: 	modified:   src/components/home/total-count-card.tsx

--- src/components/home/total-count-card.tsx
+++ src/components/home/total-count-card.tsx
@@ -1,3 +1,7 @@
+import { totalCountVariants } from '@/constants'
+import { Card, Skeleton } from 'antd'
+import { Text } from '../text'
+
 type Props = {
   resource: 'companies' | 'contacts' | 'deals'
   isLoading: boolean
@@ -9,7 +13,50 @@ const DashboardTotalCountCard = ({
   isLoading,
   totalCount,
 }: Props) => {
-  return <div>DashboardTotalCountCard</div>
+  const { primaryColor, secondaryColor, icon, title } =
+    totalCountVariants[resource]
+
+  return (
+    <Card
+      style={{ height: '96px', padding: 0 }}
+      bodyStyle={{ padding: '8px 8px 8px 16px' }}
+      size='small'
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {icon}
+        <Text size='md' className='secondary' style={{ marginLeft: '8px' }}>
+          {title}
+        </Text>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Text
+          size='xxxl'
+          strong
+          style={{
+            flex: 1,
+            whiteSpace: 'nowrap',
+            flexShrink: 0,
+            textAlign: 'start',
+            marginLeft: '48px',
+            fontVariantNumeric: 'tabular-nums',
+          }}
+        >
+          {isLoading ? (
+            <Skeleton.Button style={{ marginTop: '8px', width: '74px' }} />
+          ) : (
+            totalCount
+          )}
+        </Text>
+      </div>
+    </Card>
+  )
 }
 
 export default DashboardTotalCountCard
