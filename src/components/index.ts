ed9b483ea48e2399324a95ae08086212addf29ba
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-24 21:52:10 +0200

    Add latest actvities Changes to be committed: 	new file:   src/components/home/latest-activities.tsx 	modified:   src/components/index.ts 	modified:   src/pages/home/index.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,12 @@ import DealsChart from './home/deals-chart'
 import UpcomingEvents from './home/upcoming-events'
 import UpcomingEventsSkeleton from './skeleton/upcoming-events'
 import DashboardTotalCountCard from './home/total-count-card'
+import LatestActivities from './home/latest-activities'
 
-export { DealsChart, UpcomingEvents, UpcomingEventsSkeleton, DashboardTotalCountCard }
+export {
+  DealsChart,
+  UpcomingEvents,
+  UpcomingEventsSkeleton,
+  DashboardTotalCountCard,
+  LatestActivities,
+}
