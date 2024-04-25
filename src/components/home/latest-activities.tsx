bb48fc9b1f13a585151de65643deb30c44711cb2
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-25 21:58:48 +0200

    Add skeletor to latest activities view Changes to be committed: 	modified:   src/components/home/latest-activities.tsx

--- src/components/home/latest-activities.tsx
+++ src/components/home/latest-activities.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { UnorderedListOutlined } from '@ant-design/icons'
+import { Card, List } from 'antd'
+import { Text } from '../text'
+import LatestActivitiesSkeleton from '../skeleton/latest-activities'
 
 const LatestActivities = () => {
+  const isLoading = false
   return (
-    <div>LatestActivities</div>
+    <Card
+      headStyle={{ padding: '16px' }}
+      bodyStyle={{ padding: '0 1rem' }}
+      title={
+        <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <UnorderedListOutlined />
+          <Text size='sm' style={{ marginLeft: '0.5rem' }}>
+            Latest Activities
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout='horizontal'
+          dataSource={Array.from({ length: 3 }).map((_, id) => ({ id }))}
+          renderItem={(_,index) => (
+            <LatestActivitiesSkeleton key={index} />
+          )}
+        />
+      ) : (
+        <List />
+      )}
+    </Card>
   )
 }
 
