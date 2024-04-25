6966d1b68052775f391a71dcffc76dec4d95f4c3
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-04-25 22:34:55 +0200

    Add data to latest activities Changes to be committed: 	modified:   src/components/home/latest-activities.tsx

--- src/components/home/latest-activities.tsx
+++ src/components/home/latest-activities.tsx
@@ -1,10 +1,55 @@
 import { UnorderedListOutlined } from '@ant-design/icons'
-import { Card, List } from 'antd'
+import { Card, List, Space } from 'antd'
 import { Text } from '../text'
 import LatestActivitiesSkeleton from '../skeleton/latest-activities'
+import { useList } from '@refinedev/core'
+import {
+  DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY,
+  DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY,
+} from '@/graphql/queries'
+import dayjs from 'dayjs'
+import CustomAvatar from '../custom-avatar'
 
 const LatestActivities = () => {
-  const isLoading = false
+  const {
+    data: audits,
+    isLoading: isLoadingAudits,
+    isError,
+    error,
+  } = useList({
+    resource: 'audits',
+    meta: {
+      qglQuery: DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY,
+    },
+  })
+
+  const dealIds = audits?.data?.map((audit) => audit?.targetId)
+
+  const { data: deals, isLoading: isLoadingDeals } = useList({
+    resource: 'deals',
+    queryOptions: { enabled: !!dealIds?.length },
+    pagination: {
+      mode: 'off',
+    },
+    filters: [
+      {
+        field: 'id',
+        operator: 'in',
+        value: dealIds,
+      },
+    ],
+    meta: {
+      qglQuery: DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY,
+    },
+  })
+
+  if (isError) {
+    console.log(error)
+    return null
+  }
+
+  const isLoading = isLoadingAudits || isLoadingDeals
+
   return (
     <Card
       headStyle={{ padding: '16px' }}
@@ -22,12 +67,46 @@ const LatestActivities = () => {
         <List
           itemLayout='horizontal'
           dataSource={Array.from({ length: 3 }).map((_, id) => ({ id }))}
-          renderItem={(_,index) => (
-            <LatestActivitiesSkeleton key={index} />
-          )}
+          renderItem={(_, index) => <LatestActivitiesSkeleton key={index} />}
         />
       ) : (
-        <List />
+        <List
+          itemLayout='horizontal'
+          dataSource={audits?.data}
+          renderItem={(item) => {
+            const deal =
+              deals?.data.find((el) => el.id === String(item.targetId)) ||
+              undefined
+
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  title={dayjs(deal?.createdAt).format('MMM DD, YYYY - HH:mm')}
+                  avatar={
+                    <CustomAvatar
+                      shape='square'
+                      size={48}
+                      src={deal?.company.avatarUrl}
+                      name={deal?.company.name}
+                    />
+                  }
+                  description={
+                    <Space size={4}>
+                      <Text strong>{item.user.name}</Text>
+                      <Text>
+                        {item.action === 'CREATE' ? 'created' : 'moved'}
+                      </Text>
+                      <Text strong>{deal?.title}</Text>
+                      <Text>deal</Text>
+                      <Text>{item.action === 'CREATE' ? 'in' : 'to'}</Text>
+                      <Text strong>{deal?.stage?.title}</Text>
+                    </Space>
+                  }
+                />
+              </List.Item>
+            )
+          }}
+        />
       )}
     </Card>
   )
