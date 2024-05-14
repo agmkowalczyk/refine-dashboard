38ff04c92d221cd91f4fc0dc79b29e6118d24dc4
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-05-14 18:59:31 +0200

    Add user select to edit page Changes to be committed: 	modified:   src/pages/company/edit.tsx

--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,8 +1,12 @@
 import CustomAvatar from '@/components/custom-avatar'
+import SelectOptionWithAvatar from '@/components/select-option-with-avatar'
 import { UPDATE_COMPANY_MUTATION } from '@/graphql/mutations'
+import { USERS_SELECT_QUERY } from '@/graphql/queries'
+import { UsersSelectQuery } from '@/graphql/types'
 import { getNameInitials } from '@/utilities'
-import { Edit, useForm } from '@refinedev/antd'
-import { Col, Form, Row } from 'antd'
+import { Edit, useForm, useSelect } from '@refinedev/antd'
+import { GetFieldsFromList } from '@refinedev/nestjs-query'
+import { Col, Form, Row, Select } from 'antd'
 
 export const EditPage = () => {
   const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
@@ -13,6 +17,19 @@ export const EditPage = () => {
   })
 
   const { avatarUrl, name } = queryResult?.data?.data || {}
+
+  const { selectProps, queryResult: queryResultUsers } = useSelect<
+    GetFieldsFromList<UsersSelectQuery>
+  >({
+    resource: 'users',
+    optionLabel: 'name',
+    pagination: {
+      mode: 'off',
+    },
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
+    },
+  })
 
   return (
     <div>
@@ -30,6 +47,27 @@ export const EditPage = () => {
                 name={getNameInitials(name || '')}
                 style={{ width: 96, height: 96, marginBottom: '24px' }}
               />
+              <Form.Item
+                label='Sales owner'
+                name='salerOwnerId'
+                initialValue={formProps?.initialValues?.salesOwner?.id}
+              >
+                <Select
+                  placeholder='Please select a sales owner'
+                  {...selectProps}
+                  options={
+                    queryResultUsers.data?.data.map((user) => ({
+                      value: user.id,
+                      label: (
+                        <SelectOptionWithAvatar
+                          name={user.name}
+                          avatarUrl={user.avatarUrl ?? undefined}
+                        />
+                      ),
+                    })) ?? []
+                  }
+                />
+              </Form.Item>
             </Form>
           </Edit>
         </Col>
