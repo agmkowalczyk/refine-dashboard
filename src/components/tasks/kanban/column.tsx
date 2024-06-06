931cff7b1bac09801de5bf5cf7a3246a99ff8cc3
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-06-06 23:15:46 +0200

    Add elements to kanban column Changes to be committed: 	modified:   src/components/tasks/kanban/column.tsx

--- src/components/tasks/kanban/column.tsx
+++ src/components/tasks/kanban/column.tsx
@@ -1,9 +1,18 @@
 import { Text } from '@/components/text'
+import { PlusOutlined } from '@ant-design/icons'
 import { useDroppable } from '@dnd-kit/core'
-import { Space } from 'antd'
+import { Badge, Button, Space } from 'antd'
+import React from 'react'
 
-const KanbanColumn = () => {
+const KanbanColumn = ({ children }: React.PropsWithChildren) => {
   const { isOver, setNodeRef, active } = useDroppable({ id: '', data: '' })
+
+  const count = 2
+  const description = 'Description'
+  const title = 'Title'
+
+  const onAddClickHandler = () => {}
+
   return (
     <div
       ref={setNodeRef}
@@ -13,15 +22,42 @@ const KanbanColumn = () => {
         <Space style={{ width: '100%', justifyContent: 'space-between' }}>
           <Space>
             <Text
-              ellipsis={{ tooltip: 'TITLE TO DO' }}
+              ellipsis={{ tooltip: title }}
               size='xs'
               strong
               style={{ textTransform: 'uppercase', whiteSpace: 'nowrap' }}
             >
-              TITLE TO DO
+              {title}
             </Text>
+            {!!count && <Badge count={count} color='cyan' />}
           </Space>
+          <Button
+            shape='circle'
+            icon={<PlusOutlined />}
+            onClick={onAddClickHandler}
+          />
         </Space>
+        {description}
+      </div>
+      <div
+        style={{
+          flex: 1,
+          overflowY: active ? 'unset' : 'scroll',
+          border: '2px dashed transparent',
+          borderColor: isOver ? '#000040' : 'transparent',
+          borderRadius: '4px',
+        }}
+      >
+        <div
+          style={{
+            marginTop: '12px',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px',
+          }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   )
