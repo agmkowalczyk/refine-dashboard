d4fa646ecfb74acea1e55d62565f04ac26b0aa44
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-06-08 09:44:14 +0200

    Add kanban items list Changes to be committed: 	modified:   src/pages/tasks/list.tsx

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,12 +1,59 @@
-import { KanbanBoardContainer, KanbanBoard } from "@/components/tasks/kanban/board"
-import KanbanColumn from "@/components/tasks/kanban/column"
+import {
+  KanbanBoardContainer,
+  KanbanBoard,
+} from '@/components/tasks/kanban/board'
+import KanbanColumn from '@/components/tasks/kanban/column'
+import KanbanItem from '@/components/tasks/kanban/item'
+import { TASKS_QUERY, TASK_STAGES_SELECT_QUERY } from '@/graphql/queries'
+import { useList } from '@refinedev/core'
 
 const List = () => {
+  const { data: stages, isLoading: isLoadingStages } = useList({
+    resource: 'taskStages',
+    filters: [
+      {
+        field: 'title',
+        operator: 'in',
+        value: ['TODO', 'IN PROGRESS', 'IN REVIEW', 'DONE'],
+      },
+    ],
+    sorters: [
+      {
+        field: 'createdAt',
+        order: 'asc',
+      },
+    ],
+    meta: {
+      gqlQuery: TASK_STAGES_SELECT_QUERY,
+    },
+  })
+
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+    resource: 'tasks',
+    sorters: [
+      {
+        field: 'dueDate',
+        order: 'asc',
+      },
+    ],
+    queryOptions: {
+      enabled: !!stages
+    },
+    pagination: {
+      mode: 'off',
+    },
+    meta: {
+      gqlQuery: TASKS_QUERY,
+    },
+  })
+
   return (
     <>
       <KanbanBoardContainer>
         <KanbanBoard>
-          <KanbanColumn></KanbanColumn>
+          <KanbanColumn>
+            <KanbanItem></KanbanItem>
+          </KanbanColumn>
           <KanbanColumn></KanbanColumn>
         </KanbanBoard>
       </KanbanBoardContainer>
