0d17ac652272b6e765816010b4199813fd9f97b7
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-06-11 15:14:48 +0200

    Render unassigned kanban items Changes to be committed: 	modified:   src/components/tasks/kanban/item.tsx 	modified:   src/pages/tasks/list.tsx

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -6,11 +6,13 @@ import KanbanColumn from '@/components/tasks/kanban/column'
 import KanbanItem from '@/components/tasks/kanban/item'
 import { TASKS_QUERY, TASK_STAGES_SELECT_QUERY } from '@/graphql/queries'
 import { TaskStage } from '@/graphql/schema.types'
+import { TasksQuery } from '@/graphql/types'
 import { useList } from '@refinedev/core'
+import { GetFieldsFromList } from '@refinedev/nestjs-query'
 import React from 'react'
 
 const List = () => {
-  const { data: stages, isLoading: isLoadingStages } = useList({
+  const { data: stages, isLoading: isLoadingStages } = useList<TaskStage>({
     resource: 'taskStages',
     filters: [
       {
@@ -30,7 +32,7 @@ const List = () => {
     },
   })
 
-  const { data: tasks, isLoading: isLoadingTasks } = useList({
+  const { data: tasks, isLoading: isLoadingTasks } = useList<GetFieldsFromList<TasksQuery>>({
     resource: 'tasks',
     sorters: [
       {
@@ -59,7 +61,7 @@ const List = () => {
     const unassignedStage = tasks.data.filter((task) => task.stageId === null)
     const grouped: TaskStage[] = stages?.data.map((stage) => ({
       ...stage,
-      tasks: tasks.data.filter((task) => task.stageId.tiString() === stage.id),
+      tasks: tasks.data.filter((task) => task.stageId?.toString() === stage.id),
     }))
     return {
       unassignedStage,
@@ -79,9 +81,16 @@ const List = () => {
             count={taskStages.unassignedStage.length || 0}
             onAddClick={() => handleAddCard({ stageId: 'unassigned' })}
           >
-            <KanbanItem></KanbanItem>
+            {taskStages.unassignedStage.map((task) => (
+              <KanbanItem
+                key={task.id}
+                id={task.id}
+                data={{ ...task, stageId: 'unassigned' }}
+              >
+                {task.title}
+              </KanbanItem>
+            ))}
           </KanbanColumn>
-          <KanbanColumn></KanbanColumn>
         </KanbanBoard>
       </KanbanBoardContainer>
     </>
